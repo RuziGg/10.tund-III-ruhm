98f3e3c1239b6e2ec1d5a7307538ba16e8d83a55
Ruslan Jegorov <rusljeg@greeny.cs.tlu.ee>
2015-11-11 12:56:49 +0200

    huvialad

--- InterestsManager.class.php
+++ InterestsManager.class.php
@@ -18,9 +18,150 @@ class InterestsManager{
 	}
 	
 	
+	function addInterest($new_interest){
+		
+		// teen objekti 
+		// seal on error, ->id ja ->message
+		// voi success ja sellel on ->message
+		$response = new StdClass();
+		
+		//kas selline huviala on juba olemas
+		$stmt = $this->connection->prepare("SELECT id FROM interests WHERE name=?");
+		$stmt->bind_param("s", $new_interest);
+		$stmt->bind_result($name);
+		$stmt->execute();
+		
+		// kas sain rea andmeid
+		if($stmt->fetch()){
+			
+			// annan errori, et selline huviala olemas
+			$error = new StdClass();
+			$error->id = 0;
+			$error->message = "Huviala <strong>".$new_interest."</strong> on juba olemas!";
+			
+			$response->error = $error;
+			
+			// koik mis on parast returni enam ei kaivitata
+			return $response;
+			
+		}
+		
+		// panen eelmise paringu kinni
+		$stmt->close();
+		
+		$stmt = $this->connection->prepare("INSERT INTO interests (name) VALUES (?)");
+		$stmt->bind_param("s", $new_interest);
+		
+		// sai edukalt salvestatud
+		if($stmt->execute()){
+			
+			$success = new StdClass();
+			$success->message = "Huviala on lisatud!";
+			
+			$response->success = $success;
+			
+		}else{
+			
+			// midagi laks katki
+			$error = new StdClass();
+			$error->id = 1;
+			$error->message = "Midagi laks katki!";
+			
+			$response->error = $error;
+			
+		}
+		
+		$stmt->close();
+		
+		return $response;
+	}
+		
+	function createDropdown(){
+		
+		$html = '';
+		
+		$html .= '<select name="new_dd_selection">';
+		
+		//$html .= '<option>1</option>';
+		
+		//$html .= '<option>2</option>';
+		
+		//$html .= '<option>3</option>';
+		
+		$stmt = $this->connection->prepare("Select id, name FROM interests");
+		$stmt->bind_result($id, $name);
+		$stmt->execute();
+		
+		//iga rea kohta
+		while($stmt->fetch()){
+			
+			$html .= '<option value="'.$id.'">'.$name.'</option>';
+			
+		}
+		
+		$html .= '</select>';
+		return $html;
+		
+	}
 	
-	
-	
+	function addUserInterest($new_interest_id){
+		
+		// teen objekti 
+		// seal on error, ->id ja ->message
+		// voi success ja sellel on ->message
+		$response = new StdClass();
+		
+		//kas sellel kasutajal on see huviala
+		$stmt = $this->connection->prepare("SELECT id FROM user_interests WHERE user_id=? AND interests_id=?");
+		$stmt->bind_param("ii", $this->user_id, $new_interest_id);
+		$stmt->bind_result($interests_id);
+		$stmt->execute();
+		
+		// kas sain rea andmeid
+		if($stmt->fetch()){
+			
+			// annan errori, et selline huviala olemas
+			$error = new StdClass();
+			$error->id = 0;
+			$error->message = "Huviala on sinul juba olemas!";
+			
+			$response->error = $error;
+			
+			// koik mis on parast returni enam ei kaivitata
+			return $response;
+			
+		}
+		
+		// panen eelmise paringu kinni
+		$stmt->close();
+		
+		$stmt = $this->connection->prepare("INSERT INTO user_interests (user_id, interests_id) VALUES (?,?)");
+		$stmt->bind_param("ii", $this->user_id, $new_interest_id);
+		
+		// sai edukalt salvestatud
+		if($stmt->execute()){
+			
+			$success = new StdClass();
+			$success->message = "Huviala on lisatud!";
+			
+			$response->success = $success;
+			
+		}else{
+			
+			// midagi laks katki
+			$error = new StdClass();
+			$error->id = 1;
+			$error->message = "Midagi laks katki!";
+			
+			$response->error = $error;
+			
+		}
+		
+		$stmt->close();
+		
+		return $response;
+		
+	}
 	
 	
 	
--- data.php
+++ data.php
@@ -18,9 +18,70 @@
 	
 	//uus instants klassist
 	$InterestsManager = new InterestsManager($mysqli, $_SESSION["logged_in_user_id"]);
+	
+	
+	if(isset($_GET["new_interest"])){
+		
+		$add_new_response = $InterestsManager->addInterest($_GET["new_interest"]);
+		
+		
+	}
+	
+	if(isset($_GET["new_dd_selection"])){
+		
+		$add_new_userinterest_response = $InterestsManager->addUserInterest($_GET["new_dd_selection"]);
+		
+	}
+	
 ?>
 
 <p>
 	Tere, <?=$_SESSION["logged_in_user_email"];?> 
 	<a href="?logout=1"> Logi välja <a> 
 </p>
+
+<h2>Lisa huviala</h2>
+
+  <?php if(isset($add_new_response->error)): ?>
+  
+	<p style="color:red;">
+		<?=$add_new_response->error->message;?>
+	</p>
+  
+  <?php elseif(isset($add_new_response->success)): ?>
+	
+	<p style="color:green;" >
+		<?=$add_new_response->success->message;?>
+	</p>
+	
+  <?php endif; ?>
+
+
+<form>
+	<input name="new_interest">
+	<input type="submit">
+</form>
+
+
+<h2>Minu huvialad</h2>
+
+  <?php if(isset($add_new_userinterest_response->error)): ?>
+  
+	<p style="color:red;">
+		<?=$add_new_userinterest_response->error->message;?>
+	</p>
+  
+  <?php elseif(isset($add_new_userinterest_response->success)): ?>
+	
+	<p style="color:green;" >
+		<?=$add_new_userinterest_response->success->message;?>
+	</p>
+	
+  <?php endif; ?>
+
+<form>
+	<!-- siia jarele tuleb rippmenuu -->
+	<?=$InterestsManager->createDropdown();?>
+	<input type="submit">
+</form>
+
